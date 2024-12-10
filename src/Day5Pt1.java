70eae1e3c29b4ca39b08bfc980a4d9ed85c8b499
ravenivan1 <158862362+ravenivan1@users.noreply.github.com>
2024-12-10 13:21:19 -0500

    Update Day5Pt1.java (unfinished)

--- src/Day5Pt1.java
+++ src/Day5Pt1.java
@@ -2,34 +2,78 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> fileData = getFileData("src/Day4Input.txt");
-        ArrayList<String[]> rules = new ArrayList<>();
-        ArrayList<String[]> updates = new ArrayList<>();
+        ArrayList<String> fileData = getFileData("src/Day5Input.txt");
+        ArrayList<int[]> rules = new ArrayList<>();
+        ArrayList<int[]> updates = new ArrayList<>();
+        ArrayList<int[]> validUpdates = new ArrayList<>();
 
         for (String line : fileData) {
             if (line.contains("|")) {
                 String[] rule = line.split("\\|");
-                rules.add(rule);
+                int[] ruleConvert = new int[rule.length];
+                for (int i = 0; i < rule.length; i++) {
+                    ruleConvert[i] = Integer.parseInt(rule[i]);
+                }
+                rules.add(ruleConvert);
             }
             if (line.contains(",")) {
                 String[] update = line.split(",");
-                updates.add(update);
+                int[] updateConvert = new int[update.length];
+                for (int i = 0; i < update.length; i++) {
+                    updateConvert[i] = Integer.parseInt(update[i]);
+                }
+                updates.add(updateConvert);
             }
         }
-        
-        for (String[] update : updates) {
+
+        for (int[] update : updates) {
+            ArrayList<int[]> validRules = new ArrayList<>();
+            for (int[] rule : rules) {
+                if (checkValidRule(rule, update)) validRules.add(rule);
+            }
+            System.out.println(validRules);
+            System.out.println(checkValidUpdate(validRules, update));;
+            System.out.println();
+        }
+
+
+    }
+
+    public static boolean checkValidRule(int[] rule, int[] update) {
+        int num1 = rule[0];
+        int num2 = rule[1];
+        boolean num1Found = false;
+        boolean num2Found = false;
+        for (int num : update) {
+            if (num == num1) {
+                num1Found = true;
+            }
+            if (num == num2) {
+                num2Found = true;
+            }
+        }
+        return num1Found && num2Found;
+    }
+
+    public static boolean checkValidUpdate(ArrayList<int[]> validRules, int[] update) {
+        for (int[] rule : validRules) {
+            int indexFirst = -1;
+            int indexSecond = -1;
             for (int i = 0; i < update.length; i++) {
-                
+                if (update[i] == rule[0]) {
+                    indexFirst = i;
+                }
+                if (update[i] == rule[1]) {
+                    indexSecond = i;
+                }
             }
+            if (indexFirst > indexSecond) return false;
         }
-
-
+        return true;
     }
 
 
